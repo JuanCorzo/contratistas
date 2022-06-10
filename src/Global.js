a0e369c8abfd496a0ab5e2248e96ff80a11d1990
JUAN CORZO <www.j.corzo@hotmail.com>
2022-06-10 15:55:42 -0500

    cosas

--- src/Global.js
+++ src/Global.js
@@ -2,8 +2,8 @@ import Cookies from 'universal-cookie';
 const cookies = new Cookies(); 
 
 var Global = {
-    //url: 'https://contratistas-back-end.herokuapp.com/api/',
-    url: 'http://localhost:4000/api/',
+    url: 'https://contratistas-back-end.herokuapp.com/api/',
+    //url: 'http://localhost:4000/api/',
     consg: "Indef",
     autentica:  { 'Authorization': 'Bearer '+ cookies.get("token"),
         mapsKey: "AIzaSyDwldwLM7DOqg3NRm3UMmAgoEtdU8SaH6k"
